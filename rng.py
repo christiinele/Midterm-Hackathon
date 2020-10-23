afe182de008e7667fdc4b2396b084e7a857cb752
alee550 <alee550@my.bcit.ca>
2020-10-23 17:51:51 +0000

    added functions for dice rolls

---
+++ rng.py
@@ -0,0 +1,17 @@
+import random
+
+
+def roll_4():
+  return random.randint(1, 4)
+
+
+def roll_6():
+  return random.randint(1, 6)
+
+
+def roll_10():
+  return random.randint(1, 10)
+
+
+def roll_20():
+  return random.randint(1, 20)
